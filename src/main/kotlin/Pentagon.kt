9827aa4f8dcd81e603f9e80a803b12609423b231
vitorYghor <devictoryghor@gmail.com>
2024-04-11 11:10:37 -0300

    Generating the first correct sequence

--- src/main/kotlin/Pentagon.kt
+++ src/main/kotlin/Pentagon.kt
@@ -21,11 +21,11 @@ val graph = arrayOf(
 
 fun Int.toVertex(): String {
     return when (this) {
-        0 -> "a ->"
-        1 -> "b ->"
-        2 -> "c ->"
-        3 -> "d ->"
-        4 -> "e ->"
+        0 -> "a"
+        1 -> "b"
+        2 -> "c"
+        3 -> "d"
+        4 -> "e"
         else -> " "
     }
 }
@@ -38,51 +38,70 @@ fun Int.toVertex(): String {
 //    }
 //}
 
+fun putZero(arr: Array<Array<Int>>, pos: Int) {
+    for(i in arr.indices) {
+        arr[i][pos] = 0
+    }
+}
+
+fun Array<Array<Int>>.copyElements(): Array<Array<Int>> {
+    val copied = Array(this.size) { Array(this.size) { 0 } }
+    for(i in this.indices) {
+        for(j in this[i].indices) {
+            copied[i][j] = this[i][j]
+        }
+    }
+    return copied
+}
+
+fun formatPath(distance: String, letter: String) = "$letter = $distance -> $letter"
+
 fun generatePaths(graph: Array<Array<Int>>): String {
     var paths = mutableListOf<String>()
     for (i in graph.indices) {
-        var tempCopy1 = graph.copyOf()
+        var tempCopy1 = graph.copyElements()
         var path = ""
         path += i.toVertex()
 //        var pathCost = 0
         // fazer com que ele não volte para a letra que ele começou
-        tempCopy1.forEach { it[i] = 0 }
+        putZero(tempCopy1, i)
 
         for (j in tempCopy1[i].indices) {
-            val distance = tempCopy1[i][j]
+            val tempCopy2 = tempCopy1.copyElements()
+            val distance = tempCopy2[i][j]
             if (distance == 0) continue
-            path += distance.toString() + j.toVertex()
-            tempCopy1.forEach { it[j] = 0 }
+            path += formatPath(distance.toString(), j.toVertex(),)
+            putZero(tempCopy2, j)
 
-            for (k in tempCopy1[j].indices) {
-                val distance = tempCopy1[j][k]
+            for (k in tempCopy2[j].indices) {
+                val tempCopy3 = tempCopy2.copyElements()
+                val distance = tempCopy3[j][k]
                 if (distance == 0) continue
-                path += distance.toString() + k.toVertex()
-                tempCopy1.forEach { it[k] = 0}
+                path += formatPath(distance.toString(), k.toVertex())
+                putZero(tempCopy3, k)
 
-                for (l in tempCopy1[k].indices) {
-                    val distance = tempCopy1[k][l]
+                for (l in tempCopy3[k].indices) {
+                    val tempCopy4 = tempCopy3.copyElements()
+                    val distance = tempCopy4[k][l]
                     if (distance == 0) continue
-                    path += distance.toString() + l.toVertex()
-                    tempCopy1.forEach { it[l] = 0 }
+                    path += formatPath(distance.toString(), l.toVertex())
+                    putZero(tempCopy4, l)
 
-                    for(m in tempCopy1[l].indices) {
-                        val distance = tempCopy1[l][m]
+                    for(m in tempCopy4[l].indices) {
+                        val distance = tempCopy4[l][m]
                         if(distance == 0) continue
-                        path += distance.toString() + m.toVertex()
-                        tempCopy1.forEach { it[m] = 0 }
+                        path += formatPath(distance.toString(), m.toVertex())
 
                         // come back for when start
-                        val finalDistance = graph[m][i]
-                        path += finalDistance.toString() + m.toVertex()
+                        val finalDistance = graph[i][m]
+                        path += "${i.toVertex()} = $finalDistance"
                         paths.add(path)
-                        tempCopy1 = graph.copyOf()
+                        path = ""
                     }
                 }
             }
         }
 //            pathCost += distance
-
     }
-    return paths.joinToString("")
+    return paths.joinToString("\n")
 }
